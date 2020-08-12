aefe86035e680e9ef9b5d78cd812636ddf86d638
cebasypia <cebasypia.tskm8@gmail.com>
2020-08-12 07:04:24 +0000

    Add ping controller

---
+++ app/controllers/api/v1/ping_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class PingController < ApplicationController
+      def index
+        render json: {
+          message: 'pong'
+        }
+      end
+    end
+  end
+end
